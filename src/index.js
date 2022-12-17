12a0713814435298d947c7d67352b426e63dcff7
matomugo60 <matomugo60@gmail.com>
2022-12-17 16:02:44 +0300

    Weather app running

--- src/index.js
+++ src/index.js
@@ -1,3 +1,5 @@
+/* global $ */
+
 // Test to confirm if DOM has loaded
 
 document.addEventListener('DOMContentLoaded', (event) => {
@@ -5,7 +7,7 @@ document.addEventListener('DOMContentLoaded', (event) => {
   }); 
 
   // Code for the login option
-
+/*
   $(document).ready(function() {
     //$('#username').focus();
 
@@ -25,6 +27,8 @@ var ValidPassword = $('#password').val() === 'hgm2015'; // Password validate
         }
     });
 });
+*/
+
 // Alternative code for login information
 /*
 const loginForm = document.getElementById("login-form");
@@ -59,7 +63,7 @@ function Search(item){
                 }
     }
 }
-
+/*
 
   // The key used to acquire the OpenWeatherApp API
 
@@ -128,7 +132,61 @@ window.addEventListener('load', () => {
     });
     }
 });
+*/
 
+const api = '2491cc71cba0e545977b5a5da46a7573'
+
+// Openweathermap API. Do not share it publicly.
+
+const iconImg = document.getElementById('weather-icon');
+const loc = document.querySelector('#location');
+const tempC = document.querySelector('.c');
+const tempF = document.querySelector('.f');
+const desc = document.querySelector('.desc');
+const sunriseDOM = document.querySelector('.sunrise');
+const sunsetDOM = document.querySelector('.sunset');
+
+window.addEventListener('load', () => {
+  let long;
+  let lat;
+  // Accesing Geolocation of User
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition((position) => {
+      // Storing Longitude and Latitude in variables
+      long = position.coords.longitude;
+      lat = position.coords.latitude;
+      const base = `https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${long}&appid=${api}&units=metric`;
+
+      // Using fetch to get data
+      fetch(base)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          const { temp } = data.main;
+          const place = data.name;
+          const { description, icon } = data.weather[0];
+          const { sunrise, sunset } = data.sys;
+
+          const iconUrl = `https://openweathermap.org/img/wn/${icon}@2x.png`;
+          const fahrenheit = (temp * 9) / 5 + 32;
+
+          // Converting Epoch(Unix) time to GMT
+          const sunriseGMT = new Date(sunrise * 1000);
+          const sunsetGMT = new Date(sunset * 1000);
+
+          // Interacting with DOM to show data
+          iconImg.src = iconUrl;
+          loc.textContent = `${place}`;
+          desc.textContent = `${description}`;
+          tempC.textContent = `${temp.toFixed(2)} °C`;
+          tempF.textContent = `${fahrenheit.toFixed(2)} °F`;
+          sunriseDOM.textContent = `${sunriseGMT.toLocaleDateString()}, ${sunriseGMT.toLocaleTimeString()}`;
+          sunsetDOM.textContent = `${sunsetGMT.toLocaleDateString()}, ${sunsetGMT.toLocaleTimeString()}`;
+        });
+    });
+  }
+});
 
 // Feedback card code
 
